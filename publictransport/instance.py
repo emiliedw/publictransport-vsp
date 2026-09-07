341d72d0fdff7e35dce4503b508eef5912665595
Emilie De Witte <emilie.de.witte01@gmail.com>
2026-09-07 21:18:54 +0200

    vehicle-class is deleted

--- publictransport/instance.py
+++ publictransport/instance.py
@@ -4,7 +4,6 @@ from typing import Optional
 from .classes.depot import Depot
 from .classes.line import Line
 from .classes.trip import Trip
-from .classes.vehicle import Vehicle
 from .classes.charger import Charger
 from .classes.deadhead_trip import DeadheadTrip
 from .classes.vehicle_type import VehicleType
@@ -18,7 +17,6 @@ class ProblemInstance:
     depots: dict[str, Depot] = field(default_factory=dict)
     lines: dict[str, Line] = field(default_factory=dict)
     trips: dict[str, Trip] = field(default_factory=dict)
-    vehicles: dict[str, Vehicle] = field(default_factory=dict)
     chargers: dict[str, Charger] = field(default_factory=dict)
     deadheads: dict[tuple[str, str], DeadheadTrip] = field(default_factory=dict)  # (origin, destination) -> DeadheadTrip
     vehicle_type_params: dict[VehicleType, VehicleTypeParams] = field(default_factory=dict)
@@ -69,8 +67,6 @@ class ProblemInstance:
     def add_trip(self, trip: Trip) -> None:
         self.trips[trip.id] = trip
 
-    def add_vehicle(self, vehicle: Vehicle) -> None:
-        self.vehicles[vehicle.id] = vehicle
 
     def add_charger(self, charger: Charger) -> None:
         self.chargers[charger.id] = charger
@@ -88,9 +84,6 @@ class ProblemInstance:
 
     def get_depot(self, depot_id: str) -> Optional[Depot]:
         return self.depots.get(depot_id)
-
-    def get_vehicle(self, vehicle_id: str) -> Optional[Vehicle]:
-        return self.vehicles.get(vehicle_id)
 
     def get_line(self, line_id: str) -> Optional[Line]:
         return self.lines.get(line_id)
